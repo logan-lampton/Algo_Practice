e9a9216fe828d461f74d68bc36503e3feac8bbc2
Logan Lampton <lampton.logan@gmail.com>
2022-12-06 14:44:29 -0500

    added quickSort function

--- quick_sort.js
+++ quick_sort.js
@@ -19,4 +19,15 @@ function pivot(arr, start = 0, end = arr.length+1){
     return swapIdx;
 }
 
-pivot([4, 8, 2, 1, 5, 7, 6, 3])
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right)
+        // left side
+        quickSort(arr, left, pivotIndex - 1);
+        // right side
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
+
+quickSort([4, 8, 2, 1, 5, 7, 6, 3])
