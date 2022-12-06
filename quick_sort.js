95e766749794f67dd519db825ea1d6774022588f
Logan Lampton <lampton.logan@gmail.com>
2022-12-06 14:19:15 -0500

    added quick sort

---
+++ quick_sort.js
@@ -0,0 +1,13 @@
+function pivot(arr, start = 0, end = arr.length+1){
+    var pivot = arr[start];
+    var swapIdx = start;
+
+    for(var i = start + 1; i < arr.length; i++){
+        if(pivot > arr[i]){
+            
+        }
+    }
+    return arr[i]
+}
+
+pivot([4, 8, 2, 1, 5, 7, 6, 3])
