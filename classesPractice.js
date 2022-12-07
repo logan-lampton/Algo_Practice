5e8aae64ed27d495a31d47016f35e8453e03cf42
Logan Lampton <lampton.logan@gmail.com>
2022-12-07 14:46:49 -0500

    added expelled conditional, scores property, and addScore method to Student class

--- classesPractice.js
+++ classesPractice.js
@@ -4,13 +4,21 @@ class Student {
         this.lastName = lastName;
         this.grade = year;
         this.tardies = 0;
+        this.scores = [];
     }
     fullName() {
-        return `Your full name is ${this.firstName} ${this.lastName}.`
+        return `Your full name is ${this.firstName} ${this.lastName}.`;
     }
     markLate() {
-        this.tardies += 1
-        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times.`
+        this.tardies += 1;
+        if(this.tardies >= 3) {
+            return "YOU ARE EXPELLED!!!!";
+        }
+        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times.`;
+    }
+    addScore(score) {
+        this.scores.push(score);
+        return this.scores;
     }
 }
 
