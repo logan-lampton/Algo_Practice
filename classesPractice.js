13099ab2f00da7b156867fd0c83408e6326929d5
Logan Lampton <lampton.logan@gmail.com>
2022-12-07 14:42:28 -0500

    added tardies property and markLate method

--- classesPractice.js
+++ classesPractice.js
@@ -3,9 +3,14 @@ class Student {
         this.firstName = firstName;
         this.lastName = lastName;
         this.grade = year;
+        this.tardies = 0;
     }
     fullName() {
         return `Your full name is ${this.firstName} ${this.lastName}.`
+    }
+    markLate() {
+        this.tardies += 1
+        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times.`
     }
 }
 
