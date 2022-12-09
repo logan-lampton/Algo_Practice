6937758d582bfa13ca0c66b20216ceb557940cfd
Logan Lampton <lampton.logan@gmail.com>
2022-12-09 15:59:20 -0500

    created push method for DoublyLinkedList class

--- doublyLinkedLists.js
+++ doublyLinkedLists.js
@@ -1,13 +1,29 @@
 class Node {
     constructor(val) {
-        this.val = val
-        this.next = null
-        this.prev = null
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+        //same as singly linked list Node class, with the addition of this.prev = null
     }
 }
 
 class DoublyLinkedList {
     constructor() {
-        
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        var newNode = new Node(val);
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
     }
 }
