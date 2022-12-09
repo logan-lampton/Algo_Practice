ad0b7866f123970e65a941268f9d0b095b6ca6ea
Logan Lampton <lampton.logan@gmail.com>
2022-12-09 16:33:16 -0500

    created unshift method for DoublyLinkedList class

--- doublyLinkedLists.js
+++ doublyLinkedLists.js
@@ -55,4 +55,18 @@ class DoublyLinkedList {
         this.length--;
         return oldHead;
     }
+    //add one value to the front of the list
+    unshift() {
+        var newNode = new Node(val);
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
