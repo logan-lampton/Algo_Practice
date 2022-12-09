061c263b95270d064b439db793fb4a12b6ee5b30
Logan Lampton <lampton.logan@gmail.com>
2022-12-09 18:45:44 -0500

    created set method for DoublyLinkedList class

--- doublyLinkedLists.js
+++ doublyLinkedLists.js
@@ -91,4 +91,13 @@ class DoublyLinkedList {
         }
         return current;
     }
+    set(index, val) {
+        let foundNode = this.get(index);
+        if(foundNode != null) {
+            foundNode.val = val;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
