8e3b7e6d2130fb95b88ddfa2a48e50858be4120f
Logan Lampton <lampton.logan@gmail.com>
2022-12-09 18:42:37 -0500

    refactor get method

--- doublyLinkedLists.js
+++ doublyLinkedLists.js
@@ -71,22 +71,24 @@ class DoublyLinkedList {
     }
     get(index) {
         if(index < 0 || index >= this.length) return undefined;
+        let count, current;
+        //working from start
         if(index <= this.length/2) {
-            let count = 0;
-            let current = this.head;
+            count = 0;
+            current = this.head;
             while(count !== index) {
                 current = current.next;
                 count++;
             }
-            return current;
         } else {
-            let count = this.length - 1;
-            let current = this.tail
+            //working from end
+            count = this.length - 1;
+            current = this.tail
             while(count !== index) {
                 current = current.prev;
                 count--;
             }
-            return current;
         }
+        return current;
     }
 }
