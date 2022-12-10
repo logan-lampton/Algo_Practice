41ba1e6e562babab4926c70c8abe05beb3e8e46d
Logan Lampton <lampton.logan@gmail.com>
2022-12-10 17:15:10 -0500

    added details on how to use an array

---
+++ queue.js
@@ -0,0 +1,10 @@
+//array method
+//would just make an empty array
+// let q = [];
+// use unshift to add to the beginning and then pop from the end
+
+
+// more efficient datastructure from scratch
+// the datastructure is a linked list
+
+
