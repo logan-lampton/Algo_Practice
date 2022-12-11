1e2e0281f90d9e45965a1f93564023df5153de6a
Logan Lampton <lampton.logan@gmail.com>
2022-12-11 18:29:06 -0500

    created insert method in BinarySearchTree class

--- tree.js
+++ tree.js
@@ -29,7 +29,37 @@ class BinarySearchTree {
     constructor(){
         this.root = null;
     }
+    insert(val){
+        const newNode = new Node(val);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while(true){
+                if(val < current.val){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if(val > current.value){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
 }
 
+// example creating nodes without additional methods:
 const tree = new BinarySearchTree();
 tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
