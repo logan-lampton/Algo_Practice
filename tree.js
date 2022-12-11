2ba4882f115e87cf6280e85b4f6c4df2c9c21f33
Logan Lampton <lampton.logan@gmail.com>
2022-12-11 18:10:04 -0500

    created Node and BinarySearchTree classes

--- tree.js
+++ tree.js
@@ -4,4 +4,32 @@
 // Abstract planning out code (Abstract Syntax Tree)
 // AI
 // Folders in operating systems
+// Computer File Systems
+// JSON
 
+// Trees come up all the time
+// We will be studying binary search trees
+// Binary trees - every parent can have AT MOST two children per node (0, 1, or 2 children)
+// In a binary search tree:
+    // Every node to the left of a parent node is always less than the parent
+    // Every node to the right of a parent node is always greater than the parent
+
+
+// classes below:
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+}
+
+const tree = new BinarySearchTree();
+tree.root = new Node(10);
