9ffc63479fc143cd93f2bc2c6a57812e95377ce0
Logan Lampton <lampton.logan@gmail.com>
2022-12-13 14:34:33 -0500

    inserted classes and methods for tree

--- treeBreadthFirstSearch.js
+++ treeBreadthFirstSearch.js
@@ -1,21 +1,4 @@
-//trees are used in:
-// HTML DOM
-// Network routing
-// Abstract planning out code (Abstract Syntax Tree)
-// AI
-// Folders in operating systems
-// Computer File Systems
-// JSON
-
-// Trees come up all the time
-// We will be studying binary search trees
-// Binary trees - every parent can have AT MOST two children per node (0, 1, or 2 children)
-// In a binary search tree:
-    // Every node to the left of a parent node is always less than the parent
-    // Every node to the right of a parent node is always greater than the parent
-
-
-// classes below:
+// classes and methods from tree below
 
 class Node {
     constructor(value){
@@ -73,13 +56,3 @@ class BinarySearchTree {
         return current;
     }
 }
-
-// example creating nodes without additional methods:
-    // const tree = new BinarySearchTree();
-    // tree.root = new Node(10);
-    // tree.root.right = new Node(15);
-    // tree.root.left = new Node(7);
-    // tree.root.left.right = new Node(9);
-
-// many binary trees ignore duplicates and will get hung up if a duplicate is put in
-// this tree makes duplicates return "undefined"
