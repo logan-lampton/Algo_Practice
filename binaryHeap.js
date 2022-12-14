ba35b792fe59a01755c375ef07f7e2f84a1fe97c
Logan Lampton <lampton.logan@gmail.com>
2022-12-14 14:31:49 -0500

    added notes from instructor

---
+++ binaryHeap.js
@@ -0,0 +1,11 @@
+// very similar to a binary search tree but with some different rules
+
+// MaxBinaryHeap: parent nodes are always larger than the child nodes
+
+// MinBinaryHeap: parent nodes are always smaller than child nodes
+
+// Nodes in binary heaps can only have AT MOST two children
+// Unlike binary search trees, there is no order to the nodes (so NOT left is smaller, right is larger)
+
+// Binary Heaps are used to implement Priority Queues, which are VERY commonly used data structures
+// Binary Heaps are also used quie a bit with graph traversal algorithms
