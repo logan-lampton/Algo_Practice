b48ce6e16b35b8c5f0578bc640c839d930276ddc
Logan Lampton <lampton.logan@gmail.com>
2022-12-16 15:51:43 -0500

    notes on ways to deal with collisions

--- hashTable.js
+++ hashTable.js
@@ -72,3 +72,18 @@ function hash(key, arrayLength){
     }
     return total;
 }
+
+// Dealing with collisions
+    // Even with a large array and a great hash function, collisions are inevitable
+    // There are many strategies for dealing with collisions, but we'll focus on two:
+        // 1. Separate Chaining
+        // 2. Linear Probing
+
+// Separate Chaining
+    // With separate chaining, at each index in our array, we store values using a more sophisticated data structure (e.g. an array or a linked list).
+    // This allows us to store multiple key-value pairs at the same index.
+        // EX: index 4 reads as a nested array [["darkblue", "00008b"], ["salmon", "#fa8072"]]
+
+// Linear Probing
+    // With linear probing, when we find a collision, we search through the array to find the next empty slot.
+    // You only store one thing at each position/index
