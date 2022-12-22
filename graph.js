982311f5f187ee33004fb0bfd1ade49684c95b11
Logan Lampton <lampton.logan@gmail.com>
2022-12-21 19:19:18 -0500

    made visited[start] count as visited in breadthFirst method

--- graph.js
+++ graph.js
@@ -146,6 +146,7 @@ class Graph {
         const result = [];
         const visited = {};
         let currentVertex;
+        visited[start] = true;
 
         while(queue.length){
             currentVertex = queue.shift();
